46f1c8d619e1bd856a5d2ca746a020d005280201
Rossa McLaughlin <IcyRossa@gmail.com>
2017-02-03 12:49:19 +0000

    Initial commit

---
+++ app/src/main/java/com/example/android/masalarestaurant/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.android.masalarestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void call(View view){
+
+    }
+
+    public void directions(View view){
+
+    }
+
+    public void website(View view){
+
+    }
+}
